eb1cab3ded48f365ff480941db60fab0d81817b8
pisa0401mst <pisa0401mst@gmail.com>
2019-05-31 04:01:24 +0900

    [add] 進捗バー実装

---
+++ WPFSample/ProgressBar/ViewModel/SimpleProgressViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ProgressBar.ViewModel
+{
+    public class SimpleProgressViewModel : INotifyPropertyChanged
+    {
+        private int _progress;
+        public int Progress
+        {
+            get { return this._progress;}
+            set
+            {
+                this._progress = value;
+                this.NotifyPropertyChanged(nameof(this.Progress));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string name)
+        {
+            this.PropertyChanged?.Invoke(
+                this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
