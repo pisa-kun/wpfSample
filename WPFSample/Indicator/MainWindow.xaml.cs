6a29020dde9467ca83851e96078742b5b4929208
pisa0401mst <pisa0401mst@gmail.com>
2019-05-31 03:41:42 +0900

    [add] インジケータ実装

---
+++ WPFSample/Indicator/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Indicator
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Indicator_Loaded(object sender, RoutedEventArgs e)
+        {
+            var host = (System.Windows.Forms.Integration.WindowsFormsHost)sender;
+            var pbox = (System.Windows.Forms.PictureBox)host.Child;
+            //loading.gifのビルドアクションをResourcesに
+            //参考サイトはパスを"Resources/"にしているがSystem.IO.Exceptionが返ってくるので"/Resources/"が正解
+            pbox.Image = System.Drawing.Image.FromStream(Application.GetResourceStream(new Uri("/Resources/loading.gif", UriKind.Relative)).Stream);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,43 @@
+WPF Sample
+====
+リソースからアニメーションGIF :
+[https://mitosuya.net/show-gif-from-resource]
+
+ProgressBar : 
+[https://webbibouroku.com/Blog/Article/wpf-progressbar]
+
+ProgressBar : 
+[https://anderson02.com/cs/wpf/wpf-19/]
+
+ローディングGIF :
+[https://ferret-plus.com/3878]
+
+- Overview
+ 
+## Description
+
+## Demo
+
+## VS. 
+
+## Requirement
+
+## Usage
+
+## Install
+
+## Contribution
+
+## Licence
+
+[MIT](https://github.com/tcnksm/tool/blob/master/LICENCE)
+
+## Author
+
+[tcnksm](https://github.com/tcnksm)
+
+```plantuml
+@startuml
+Title シーケンス図
+Bob->Alice :Hello
+@enduml
